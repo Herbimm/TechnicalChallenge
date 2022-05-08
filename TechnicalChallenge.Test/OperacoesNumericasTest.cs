bafd0a026d77635a0394563dedc08ff562487dae
herbert.carvalho <herbertzin@gmail.com>
2022-05-08 13:43:43 -0300

    End of tests

--- TechnicalChallenge.Test/OperacoesNumericasTest.cs
+++ TechnicalChallenge.Test/OperacoesNumericasTest.cs
@@ -59,6 +59,79 @@ namespace TechnicalChallenge.Test
             Assert.Equal(numero.Divisores,numerosInteirosDivididos);            
         }
 
+        [Theory]
+        [InlineData(1)]
+        public void NaoDeveRetornarPrimosSeValorForUm(decimal numeroDigitado)
+        {
+            NumeroOperacional numero = new NumeroOperacional();
+            numero.Numero = numeroDigitado;
+            if (numero.Numero <= 1)
+            {
+                numero.NumerosPrimos = null;
+            }
+            Assert.Equal(numero.NumerosPrimos, null);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public void UnicoNumeroParQueRetornaPrimo(decimal numeroDigitado)
+        {
+            NumeroOperacional numero = new NumeroOperacional();
+            numero.Numero = numeroDigitado;
+            if (numero.Numero == 2)
+            {                
+                numero.NumerosPrimos.Add(1);                
+                numero.NumerosPrimos.Add(2);                
+            }
+            Assert.Equal(numero.NumerosPrimos, new List<int> { 1,2 });
+        }
+
+        [Theory]
+        [InlineData(45)]
+        public void DeveRetornarNumerosPrimosDosDivisoresPossiveis(decimal numeroDigitado)
+        {
+            var divisores = RetornaNumerosDivisiveis(numeroDigitado);
+            NumeroOperacional numero = new NumeroOperacional();
+            numero.Numero = numeroDigitado;
+            List<int> numerosPrimos = new List<int>();
+            foreach (var item in divisores)
+            {
+                List<int> contagemNumerosDivisiveis = new List<int>();
+
+                for (int i = 1; i < item + 1; i++)
+                {
+                    decimal divisaoResultado = Convert.ToDecimal(item) / i;
+                    string result = divisaoResultado.ToString();
+                    if (!Convert.ToString(result).Contains(","))
+                    {
+                        contagemNumerosDivisiveis.Add(i);
+                    }
+                }
+                if (contagemNumerosDivisiveis.Count == 2)
+                {
+                    numerosPrimos.Add(item);
+                }
+            }
+            numero.NumerosPrimos = numerosPrimos;
+            Assert.Equal(numero.NumerosPrimos, new List<int> { 3, 5 });
+            Assert.Equal(numero.NumerosPrimos, numerosPrimos);
+        }
+
+        private List<int> RetornaNumerosDivisiveis(decimal numero)
+        {
+            List<int> numerosInteirosDivididos = new List<int>();
+            for (int i = 1; i < numero + 1; i++)
+            {
+                decimal divisaoResultado = numero / i;
+                string result = divisaoResultado.ToString();
+                if (!Convert.ToString(result).Contains(","))
+                {
+                    numerosInteirosDivididos.Add(i);
+                }
+            }
+            return numerosInteirosDivididos;
+        }
+
 
     }
 }
